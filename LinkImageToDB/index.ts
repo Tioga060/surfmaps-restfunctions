3e45cb9d8853db21998572f3e21fe60b955c2756
Tioga060 <jeffhanford060@gmail.com>
2019-05-13 21:15:53 -0500

    quick push deploy test

--- LinkImageToDB/index.ts
+++ LinkImageToDB/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context } from "@azure/functions"
+import {Pool} from 'pg';
 
 const eventGridTrigger: AzureFunction = async function (context: Context, eventGridEvent: any): Promise<void> {
     context.log(typeof eventGridEvent);
