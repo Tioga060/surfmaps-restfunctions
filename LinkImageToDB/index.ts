4a7d194d89b4cd010b0cfe03bddf0177d6412a56
Tioga060 <jeffhanford060@gmail.com>
2019-05-14 23:24:40 -0500

    fix image type gathering

--- LinkImageToDB/index.ts
+++ LinkImageToDB/index.ts
@@ -1,9 +1,13 @@
 import { AzureFunction, Context } from "@azure/functions"
 import {Pool} from 'pg';
 
-const ALLOWED_IMAGE_TYPES = [
-    'image/jpeg',
-    'image/png',
+const ALLOWED_IMAGE_EXTENSIONS = [
+    'jpeg',
+    'JPEG',
+    'jpg',
+    'JPG',
+    'png',
+    'PNG',
 ];
 
 interface IEventGridEvent {
@@ -34,7 +38,8 @@ const eventGridTrigger: AzureFunction = async function (context: Context, eventG
     if (eventGridEvent && eventGridEvent.data && eventGridEvent.data.url) {
         const fileUrl = eventGridEvent.data.url;
         const eventType = eventGridEvent.eventType;
-        if (eventType === EVENT_TYPE && ALLOWED_IMAGE_TYPES.includes(eventGridEvent.data.contentType)) {
+        const isCorrectDataType = ALLOWED_IMAGE_EXTENSIONS.some(extension => (fileUrl.endsWith(extension)));
+        if (eventType === EVENT_TYPE && isCorrectDataType) {
             const [mapId, imageId] = fileUrl.split('/').slice(3);
             if (mapId && mapId.length === 36 && imageId && imageId.length === 36) {
                 const pool = new Pool({
